02b7a5f5dbc845ca6c38f7729b789a59ebb51af9
robthemonster <robthemonsterlevin@gmail.com>
2018-02-09 22:47:29 -0500

    Initial commit

---
+++ src/ClientInfo.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+public class ClientInfo implements Serializable {
+    private String firebaseToken;
+    private String message;
+
+    public String getFirebaseToken() {
+        return firebaseToken;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public void setFirebaseToken(String firebaseToken){
+        this.firebaseToken = firebaseToken;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
+
