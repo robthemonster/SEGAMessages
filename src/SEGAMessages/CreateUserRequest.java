87097113ce3cc9be276de6540b357f1f1fee15e0
robthemonster <robthemonsterlevin@gmail.com>
2018-02-21 19:54:06 -0500

    added new request type for creating a user

---
+++ src/SEGAMessages/CreateUserRequest.java
@@ -0,0 +1,24 @@
+package SEGAMessages;
+
+import java.io.Serializable;
+
+public class CreateUserRequest implements Serializable {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
