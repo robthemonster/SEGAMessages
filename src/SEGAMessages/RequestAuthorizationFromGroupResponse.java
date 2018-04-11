3b57cc18da386d73fdafe251e784fb4f5ba1a359
robthemonster <robthemonsterlevin@gmail.com>
2018-04-11 17:32:40 -0400

    Authorization requests now return tokens!

--- src/SEGAMessages/RequestAuthorizationFromGroupResponse.java
+++ src/SEGAMessages/RequestAuthorizationFromGroupResponse.java
@@ -3,6 +3,23 @@ package SEGAMessages;
 public class RequestAuthorizationFromGroupResponse extends Response {
     public static final String TYPE = "RequestAuthorizationFromGroupResponse";
     private boolean succeeded;
+    private String token;
+
+    @Override
+    public String toString() {
+        return "RequestAuthorizationFromGroupResponse{" +
+                "succeeded=" + succeeded +
+                ", token='" + token + '\'' +
+                '}';
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     public boolean isSucceeded() {
         return succeeded;
@@ -10,13 +27,6 @@ public class RequestAuthorizationFromGroupResponse extends Response {
 
     public void setSucceeded(boolean succeeded) {
         this.succeeded = succeeded;
-    }
-
-    @Override
-    public String toString() {
-        return "RequestAuthorizationFromGroupResponse{" +
-                "succeeded=" + succeeded +
-                '}';
     }
 
     @Override
